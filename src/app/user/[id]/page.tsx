6d43dea47e83d2c3f851a34543233d61548a2edf
Kota <koke.kokota0130@gmail.com>
2023-06-18 01:01:27 +0900

    fix environment parameter

--- src/app/user/[id]/page.tsx
+++ src/app/user/[id]/page.tsx
@@ -1,7 +1,7 @@
 import UserContainer from "@/components/User/UserContainer";
 
 const page = async ({ params }: { params: { id: string } }) => {
-  const url = process.env.VERCEL_URL || "http://localhost:3000";
+  const url = process.env.NEXT_PUBLIC_VERCEL_URL || "http://localhost:3000";
   const user = await fetch(`${url}/api/user/${params.id}`, {
     cache: "force-cache",
   }).then((res) => res.json());
