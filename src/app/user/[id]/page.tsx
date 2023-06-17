64ceb793f39be0c3cad32af377e10d341c0de055
Kota <koke.kokota0130@gmail.com>
2023-06-18 01:22:30 +0900

    url fix

--- src/app/user/[id]/page.tsx
+++ src/app/user/[id]/page.tsx
@@ -1,7 +1,9 @@
 import UserContainer from "@/components/User/UserContainer";
 
 const page = async ({ params }: { params: { id: string } }) => {
-  const url = process.env.NEXT_PUBLIC_VERCEL_URL || "http://localhost:3000";
+  const url = process.env.NEXT_PUBLIC_VERCEL_URL
+    ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
+    : "http://localhost:3000";
   const user = await fetch(`${url}/api/user/${params.id}`, {
     cache: "force-cache",
   }).then((res) => res.json());
