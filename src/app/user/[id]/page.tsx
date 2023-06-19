94822901dead35b3c83f32c0c117e73195a432d6
Kota <koke.kokota0130@gmail.com>
2023-06-19 14:31:29 +0900

    parameter fixed

--- src/app/user/[id]/page.tsx
+++ src/app/user/[id]/page.tsx
@@ -1,10 +1,9 @@
 import UserContainer from "@/components/User/UserContainer";
 
-const url = process.env.NEXT_PUBLIC_VERCEL_URL
-  ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
-  : "http://localhost:3000";
-
 export async function generateStaticParams() {
+  const url = process.env.NEXT_PUBLIC_VERCEL_URL
+    ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
+    : "http://localhost:3000";
   const data = await fetch(`${url}/api/user`)
     .then((res) => {
       if (!res.ok) throw new Error(`${res.status}`);
@@ -19,6 +18,9 @@ export async function generateStaticParams() {
 }
 
 const page = async ({ params }: { params: { id: string } }) => {
+  const url = process.env.NEXT_PUBLIC_VERCEL_URL
+    ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
+    : "http://localhost:3000";
   const user = await fetch(`${url}/api/user/${params.id}`, {
     cache: "force-cache",
   })
