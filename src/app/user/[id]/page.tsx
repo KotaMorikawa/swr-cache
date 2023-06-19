e5a3bc551f0fa3218db2d83c1fb6829b58cb0f38
Kota <koke.kokota0130@gmail.com>
2023-06-19 14:42:24 +0900

    framework error?

--- src/app/user/[id]/page.tsx
+++ src/app/user/[id]/page.tsx
@@ -1,36 +1,13 @@
 import UserContainer from "@/components/User/UserContainer";
 
-export async function generateStaticParams() {
-  const url = process.env.NEXT_PUBLIC_VERCEL_URL
-    ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
-    : "http://localhost:3000";
-  const data = await fetch(`${url}/api/user`)
-    .then((res) => {
-      if (!res.ok) throw new Error(`${res.status}`);
-      return res.json();
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-  return data.map((user: { id: number }) => ({
-    id: String(user.id),
-  }));
-}
+const url = process.env.NEXT_PUBLIC_VERCEL_URL
+  ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
+  : "http://localhost:3000";
 
 const page = async ({ params }: { params: { id: string } }) => {
-  const url = process.env.NEXT_PUBLIC_VERCEL_URL
-    ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
-    : "http://localhost:3000";
   const user = await fetch(`${url}/api/user/${params.id}`, {
     cache: "force-cache",
-  })
-    .then((res) => {
-      if (!res.ok) throw new Error(`${res.status}`);
-      return res.json();
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+  }).then((res) => res.json());
 
   return <UserContainer user={user} />;
 };
