546164f57c755d614e28cbeff345d3b803df1fb7
Kota <koke.kokota0130@gmail.com>
2023-06-19 14:21:22 +0900

    SSG(URL fix)

--- src/app/user/[id]/page.tsx
+++ src/app/user/[id]/page.tsx
@@ -4,10 +4,31 @@ const url = process.env.NEXT_PUBLIC_VERCEL_URL
   ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
   : "http://localhost:3000";
 
+export async function generateStaticParams() {
+  const data = await fetch(`${url}/api/user`)
+    .then((res) => {
+      if (!res.ok) throw new Error(`${res.status}`);
+      return res.json();
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+  return data.map((user: { id: number }) => ({
+    id: String(user.id),
+  }));
+}
+
 const page = async ({ params }: { params: { id: string } }) => {
   const user = await fetch(`${url}/api/user/${params.id}`, {
     cache: "force-cache",
-  }).then((res) => res.json());
+  })
+    .then((res) => {
+      if (!res.ok) throw new Error(`${res.status}`);
+      return res.json();
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 
   return <UserContainer user={user} />;
 };
